ce9fadaa441f92676ca55bce97620cef23b9388d
Zippy6666 <simperzippy@gmail.com>
2024-12-10 12:28:50 +0100

    will i even use this

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
+if __name__ != "__main__": raise ImportError("This module cannot be imported.")
+
+
 from typing import Any
 import pandas as pd
 import numpy as np
 
 
-# Load the CSV file into a DataFrame
-_df = pd.read_csv("lagerstatus.csv")
-
-
 class DeliveryVan:
-    def __init__(self):
+    def __init__(self, name):
         """Create a new delivery van."""
-    
+        self._loaded_weight = 0
+        self._name = name
+
+    def get_loaded_weight(self):
+        """Get how many kilograms worth of packages are loaded into the van."""
+        return self._loaded_weight
+
     def load_package():
         """Load a package into this van."""
         raise NotImplementedError()
-
-
-_delivery_vans = (DeliveryVan() for _ in range(10))
+    
+    def __repr__(self):
+        return self._name
         
 
-def get_delivery_van(package) -> Any:
+def decide_delivery_van(package) -> Any:
     """
     Determines which van should haul the given package.
 
@@ -37,9 +42,14 @@ def get_delivery_van(package) -> Any:
 
 
 def main():
-    selected_row = _df[_df["Paket_id"] == 2472920751]
-    print(type(selected_row))
-    print(selected_row)
+    df = pd.read_csv("lagerstatus.csv") # Dataframe
+    delivery_vans = [DeliveryVan(f"bil_{i+1}") for i in range(10)]
+
+    print(delivery_vans)
+
+    # selected_row = df[df["Paket_id"] == 2472920751]
+    # print(type(selected_row))
+    # print(selected_row)
 
 
 if __name__ == "__main__":
