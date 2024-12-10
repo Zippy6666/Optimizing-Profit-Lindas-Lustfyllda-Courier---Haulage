b3c99085a6c9e047505a2556541c0b649c46eaf3
Zippy6666 <simperzippy@gmail.com>
2024-12-10 15:56:48 +0100

    progress

--- main.py
+++ main.py
@@ -1,47 +1,49 @@
-from typing import Any
 import pandas as pd
 import numpy as np
-import os
-
-
-class Package:
-    def __init__(self, id_, weight, profit, deadline):
-        self._id = id_
-        self._weight = weight
-        self._profit = profit
-        self._deadline = deadline
+import os, random
 
 
 class _DeliveryVan:
     def __init__(self, name):
         """Create a new delivery van."""
+        
         assert __name__ == "__main__", "Don't import this class."
 
-        self._loaded_weight = 0
-        self._packages: list[Package] = []
+        self._loaded_weight = 0.0
+        self._packages: np.array = np.array([], dtype=object)
         self._name = name
 
-    def load_package(self, id_):
-        """Load a package into this van. The total weight of all the van's packages cannot exceed 800 Kg."""
+    def load_package(self, package: pd.Series):
+        """
+        Load a package into this van. The total weight cannot exceed 800 Kg.
+
+        ### Args:
+        - `package`: A row from the `lagerstatus.csv` file containing package details.
+        """
+
+        weight = package["Vikt"]
 
         assert self._loaded_weight + weight < 800, "Too many packages in this van."
 
-        selected_row = _df[_df["Paket_id"] == id_]
-        weight, profit, deadline = selected_row["Vikt"], selected_row["Förtjänst"], selected_row["Deadline"]
-
-        self._packages.append( Package(id_, weight, profit, deadline) )
-        self._loaded_weight += weight
-
-
-def _decide_delivery_van(package: Package) -> _DeliveryVan:
-    """ Use an algorithm to decide which van this package should be contained in. """
-    raise NotImplementedError()
+        self._loaded_weight = round(self._loaded_weight + weight, 1)
+        self._packages = np.append(self._packages, package)
+    
+    def __repr__(self):
+        return self._name+f" [{self._loaded_weight} Kg]"
 
 
-def _get_total_delivery_weight() -> float:
-    """Get the summed weight of all packages to be delivered"""
+def _decide_delivery_van(package: pd.Series) -> _DeliveryVan:
+    """
+    Use an algorithm to decide which van this package should be contained in.
+
+    ### Args:
+    - `package`: A row from the `lagerstatus.csv` file containing package details.
+    """
     assert __name__ == "__main__", "Don't import this function."
-    return _df["Vikt"].sum()
+    
+    # Stupid algorithm that just puts it in a random van:
+    van = random.choice(_delivery_vans)
+    return van
 
 
 def main():
@@ -53,9 +55,13 @@ def main():
     global _delivery_vans
 
     _df = pd.read_csv("lagerstatus.csv") # Dataframe
-    _delivery_vans = [_DeliveryVan(f"bil_{i+1}") for i in range(10)]
+    _delivery_vans = [_DeliveryVan(f"bil_{i+1}") for i in range(10)] # 10 vans
 
-    print( _get_total_delivery_weight() )
+    for _, package in _df.iterrows():
+        van = _decide_delivery_van(package)
+        van.load_package(package)
+        print("Loaded package", package["Paket_id"], "into", van, ".")
+
 
 if __name__ == "__main__":
     main()
