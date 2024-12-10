0cfa68b931e5cc0e954ae8ab6d6555b94ce0f73c
Zippy6666 <simperzippy@gmail.com>
2024-12-10 10:02:15 +0100

    professional function writing

---
+++ main.py
@@ -0,0 +1,18 @@
+from collections.abc import Any
+
+
+def get_delivery_van(package) -> Any:
+    """
+    Determines which van should haul the given package.
+
+    Parameters
+    ----------
+    package : Any
+        The package to check.
+
+    Returns
+    -------
+    Any
+        The van the package should be hauled with.
+    """
+    ...
