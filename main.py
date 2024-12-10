5b07203c88a6cce28e8636cddac817c8f178863c
Zippy6666 <simperzippy@gmail.com>
2024-12-10 11:55:24 +0100

    fortnite INSANE

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from typing import Any
+import pandas as pd
+import numpy as np
+
+
+# Load the CSV file into a DataFrame
+_df = pd.read_csv("lagerstatus.csv")
 
 
 class DeliveryVan:
     def __init__(self):
         """Create a new delivery van."""
+    
+    def load_package():
+        """Load a package into this van."""
+        raise NotImplementedError()
 
 
 _delivery_vans = (DeliveryVan() for _ in range(10))
@@ -27,7 +37,9 @@ def get_delivery_van(package) -> Any:
 
 
 def main():
-    raise NotImplementedError()
+    selected_row = _df[_df["Paket_id"] == 2472920751]
+    print(type(selected_row))
+    print(selected_row)
 
 
 if __name__ == "__main__":
