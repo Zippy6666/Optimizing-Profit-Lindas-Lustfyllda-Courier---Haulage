a233e78eb0ad0fcb5c19971fd4051361ca53584a
Zippy6666 <simperzippy@gmail.com>
2024-12-11 16:57:52 +0100

    smarter storage with dataframe in van class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import os, random
+import os
 
 
 class _DeliveryVan:
@@ -9,9 +9,16 @@ class _DeliveryVan:
         
         assert __name__ == "__main__", "Don't import this class."
 
-        self._loaded_weight = 0.0
-        self._packages: np.array = np.array([], dtype=object)
         self._name = name
+        self._loaded_weight = 0.0
+
+        dtypes = {
+            "Paket_id": str,
+            "Vikt": float,
+            "Förtjänst": int,
+            "Deadline": int
+        }
+        self._packages = pd.DataFrame(columns=["Paket_id", "Vikt", "Förtjänst", "Deadline"]).astype(dtypes) # Empty DataFrame
 
     def load_package(self, package: pd.Series):
         """
@@ -26,8 +33,17 @@ class _DeliveryVan:
         assert self._loaded_weight + weight < 800, "Too many packages in this van."
 
         self._loaded_weight = round(self._loaded_weight + weight, 1)
-        self._packages = np.append(self._packages, package)
-    
+        self._packages = pd.concat([self._packages, package.to_frame().T], ignore_index=True) # Add package to van
+
+        global _df
+        _df = _df[_df["Paket_id"] != package["Paket_id"]]
+
+    def get_weight(self) -> np.float64:
+        return self._loaded_weight
+
+    def get_profit(self) -> int:
+        return self._packages["Förtjänst"].sum()
+
     def __repr__(self):
         return self._name+f" [{self._loaded_weight} Kg]"
 
@@ -40,27 +56,25 @@ def _decide_delivery_van(package: pd.Series) -> _DeliveryVan:
     - `package`: A row from the `lagerstatus.csv` file containing package details.
     """
     assert __name__ == "__main__", "Don't import this function."
-    
-    # Stupid algorithm that just puts it in a random van:
-    van = random.choice(_delivery_vans)
-    return van
+    raise NotImplementedError()
 
 
 def main():
+    # Seed new data if there isn't any
     if not os.path.isfile("lagerstatus.csv"):
         import seeder
-        seeder.seed_packages(5000)
+        seeder.seed_packages(1_360_000)
 
     global _df
+    _df = pd.read_csv("lagerstatus.csv", dtype={
+        "Paket_id": str,
+        "Vikt": float,
+        "Förtjänst": int,
+        "Deadline": int
+    })
+
     global _delivery_vans
-
-    _df = pd.read_csv("lagerstatus.csv") # Dataframe
     _delivery_vans = [_DeliveryVan(f"bil_{i+1}") for i in range(10)] # 10 vans
-
-    for _, package in _df.iterrows():
-        van = _decide_delivery_van(package)
-        van.load_package(package)
-        print("Loaded package", package["Paket_id"], "into", van, ".")
 
 
 if __name__ == "__main__":
