6cdb1e499a829bedfb6a1f1d0a11845e14adbbb5
Zippy6666 <simperzippy@gmail.com>
2024-12-18 15:04:55 +0100

    calculate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
-import os
+import seeder
+import random
+from tqdm import tqdm
 
 
 class _DeliveryVan:
@@ -11,14 +13,9 @@ class _DeliveryVan:
 
         self._name = name
         self._loaded_weight = 0.0
+        self._profit = 0
 
-        dtypes = {
-            "Paket_id": str,
-            "Vikt": float,
-            "Förtjänst": int,
-            "Deadline": int
-        }
-        self._packages = pd.DataFrame(columns=["Paket_id", "Vikt", "Förtjänst", "Deadline"]).astype(dtypes) # Empty DataFrame
+        self.is_overloaded = False
 
     def load_package(self, package: pd.Series) -> None:
         """
@@ -28,30 +25,22 @@ class _DeliveryVan:
         - `package`: A row from the `lagerstatus.csv` file containing package details.
         """
 
-        weight = package["Vikt"]
-
-        assert self._loaded_weight + weight < 800, "Too many packages in this van."
-
-        self._loaded_weight = round(self._loaded_weight + weight, 1)
-        self._packages = pd.concat([self._packages, package.to_frame().T], ignore_index=True) # Add package to van
-
-        global _df
-        _df = _df[_df["Paket_id"] != package["Paket_id"]]
+        self._loaded_weight = round(self._loaded_weight + package["Vikt"], 1)
+        self._profit += _get_real_profit(package)
 
     def get_weight(self) -> np.float64:
         return self._loaded_weight
 
     def get_profit(self) -> int:
-        return self._packages["Förtjänst"].sum()
+        return self._profit
 
     def __repr__(self) -> str:
         return self._name+f" [{self._loaded_weight} Kg]"
+    
 
-
-def _get_real_profit(package: pd.Series) -> int:
+def _get_real_profit(package: dict) -> int:
     """
     Get the real profit with deadline taken into account for the given package.
-
     ### Args:
     - `package`: A row from the `lagerstatus.csv` file containing package details.
     """
@@ -59,23 +48,54 @@ def _get_real_profit(package: pd.Series) -> int:
     return package["Förtjänst"] - (deadline**2 if deadline < 0 else 0)
 
 
+def _feed_algorithm(chromosome: list) -> list:
+    """
+    ### Args:
+    - `chromosome`- A list representing each package as an integer that acts as the index for the van it should be put into, where -1 means don't package it into a van
+    ### Returns:
+    - `new_chromosome` An integer describing the fitness.
+    """
+
+    number_of_overloaded_vans = 0
+    package_array = _df.to_numpy()
+    column_map = {name: idx for idx, name in enumerate(_df.columns)}
+
+    for list_index, van_index in tqdm(enumerate(chromosome), total=len(chromosome), desc="Processing"):
+        if van_index != -1:
+            van: _DeliveryVan = _delivery_vans[van_index]
+            package = package_array[list_index]
+            van.load_package({
+                "Vikt": package[column_map["Vikt"]],
+                "Förtjänst": package[column_map["Förtjänst"]],
+                "Deadline": package[column_map["Deadline"]],
+            })
+
+            if not van.is_overloaded and van.get_weight() > 800:
+                number_of_overloaded_vans += 1
+                van.is_overloaded = True
+
+    total_profit = sum(van.get_profit() for van in _delivery_vans) # The profit by all vans with the deadlines taken into account
+    overload_penelty = 1_000_000*number_of_overloaded_vans
+    fitness = total_profit - overload_penelty
+
+    return ...
+
+
 def main() -> None:
-    # Seed new data if there isn't any
-    if not os.path.isfile("lagerstatus.csv"):
-        import seeder
-        seeder.seed_packages(1_360_000)
+    print("Seeding new packages...")
+
+    # Seed new data
+    seeder.seed_packages(1_360_000)
+
+    print("Seeding done!")
 
     global _df
-    _df = pd.read_csv("lagerstatus.csv", dtype={
-        "Paket_id": str,
-        "Vikt": float,
-        "Förtjänst": int,
-        "Deadline": int
-    })
-
     global _delivery_vans
+
+    _df = pd.read_csv("lagerstatus.csv", dtype={"Paket_id": str, "Vikt": float, "Förtjänst": int, "Deadline": int})
     _delivery_vans = [_DeliveryVan(f"bil_{i+1}") for i in range(10)] # 10 vans
 
+    # chromosome = [random.randint(-1, 9) for _ in range(len(_df))]
 
 if __name__ == "__main__":
     main()
--- seeder.py
+++ seeder.py
@@ -1,6 +1,8 @@
 import csv
 import random
 from pathlib import Path
+from tqdm import tqdm
+
 
 def seed_packages(n_iter: int = 100, target_path: Path = Path('lagerstatus.csv')) -> None:
     assert 0 < n_iter, 'n_iter needs to be a positive integer'
@@ -8,7 +10,7 @@ def seed_packages(n_iter: int = 100, target_path: Path = Path('lagerstatus.csv')
 
     entries = []
     id_num = random.randint(1_000_000_000, 9_999_999_999 - n_iter)
-    for _ in range(n_iter):
+    for _ in tqdm( range(n_iter), total=n_iter, desc="Seeding..." ):
         id_num += 1
         weight = round((random.randint(10, 150) + random.randint(10, 80)) / 20, 1)
         profit = int((random.randint(1, 10) + random.randint(1, 10)) / 2)
@@ -22,11 +24,14 @@ def seed_packages(n_iter: int = 100, target_path: Path = Path('lagerstatus.csv')
             }
         )
 
+    print("Writing CSV...")
+
     fieldnames = 'Paket_id','Vikt','Förtjänst','Deadline'
     with target_path.open('w', encoding='utf-8') as file:
         writer = csv.DictWriter(file, fieldnames=fieldnames, lineterminator='\n')
         writer.writeheader()
         writer.writerows(entries)
+
 
 if __name__ == '__main__':
     seed_packages()
